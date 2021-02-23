71d83c975430b4f0c73903c36d004cdf033c018c
Kristen Picard <kristenpicard93@gmail.com>
2021-02-23 12:43:47 -0500

    Color coding time blocks dependent on current time

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,83 @@
 var currentDate = moment().format('dddd, MMM DD, YYYY');
   $('#currentDay').text(currentDate);
 
+
+
+  window.onload = function(){
+    colorCode();
+   }
+  
+  var now = new Date().getHours();
+  
+  function colorCode() {
+    if (now > 8) {
+      $("#textEight").addClass("past");
+    } else if (now >= 8 && now < 9) {
+      $("#textEight").addClass("present");
+    } else if (now < 8) {
+      $("#textEight").addClass("future");
+    }
+    if (now > 9) {
+      $("#textNine").addClass("past");
+    } else if (now >= 9 && now < 10) {
+      $("#textNine").addClass("present");
+    } else if (now < 9) {
+      $("#textNine").addClass("future");
+    }
+    if (now > 10) {
+      $("#textTen").addClass("past");
+    } else if (now >= 10 && now < 11) {
+      $("#textTen").addClass("present");
+    } else if (now < 10) {
+      $("#textTen").addClass("future");
+    }
+    if (now > 11) {
+      $("#textEleven").addClass("past");
+    } else if (now >= 11 && now < 12) {
+      $("#textEleven").addClass("present");
+    } else if (now < 11) {
+      $("#textEleven").addClass("future");
+    }
+    if (now > 12) {
+      $("#textTwelve").addClass("past");
+    } else if (now >= 12 && now < 13) {
+      $("#textTwelve").addClass("present");
+    } else if (now < 12) {
+      $("#textTwelve").addClass("future");
+    }
+    if (now > 13) {
+      $("#textOne").addClass("past");
+    } else if (now >= 13 && now < 14) {
+      $("#textOne").addClass("present");
+    } else if (now < 13) {
+      $("#textOne").addClass("future");
+    }
+    if (now > 14) {
+      $("#textTwo").addClass("past");
+    } else if (now >= 14 && now < 15) {
+      $("#textTwo").addClass("present");
+    } else if (now < 14) {
+      $("#textTwo").addClass("future");
+    }
+    if (now > 15) {
+      $("#textThree").addClass("past");
+    } else if (now >= 15 && now < 16) {
+      $("#textThree").addClass("present");
+    } else if (now < 15) {
+      $("#textThree").addClass("future");
+    }
+    if (now > 16) {
+      $("#textFour").addClass("past");
+    } else if (now >= 16 && now < 17) {
+      $("#textFour").addClass("present");
+    } else if (now < 16) {
+      $("#textFour").addClass("future");
+    }
+    if (now > 17) {
+      $("#textFive").addClass("past");
+    } else if (now >= 17 && now < 18) {
+      $("#textFive").addClass("present");
+    } else if (now < 17) {
+      $("#textFive").addClass("future");
+    }
+  };
