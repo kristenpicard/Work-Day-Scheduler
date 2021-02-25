a8b3553363a502f7f5aa642341c2ddacaa28c86d
Kristen Picard <kristenpicard93@gmail.com>
2021-02-25 13:31:28 -0500

    Figured out how to complete one time block with local storage

--- Develop/script.js
+++ Develop/script.js
@@ -19,11 +19,31 @@ function colorCode() {
 colorCode();
 
 // This creates an array of the textarea ids.
-var $array = [];
-$('textarea').each(function(){
-    var id = $(this).attr('id');
-    $array.push(id);
-});
-console.log($array);
-// My thought is to call a function at each loop through the array?
-$array.forEach(myFunction);
+// var array = [];
+// $('textarea').each(function(){
+//     var id = $(this).attr('id');
+//     array.push(id);
+// });
+// console.log(array);
+
+// Figured out how to save for first time block.  Want to be able to do this for each one wil a loop?
+function eightOclock() {
+  var input = document.querySelector("#text-8");
+  var output = document.querySelector("#text-8");
+  var save = document.querySelector("#bttn-8");
+  save.addEventListener("click", updateOutput);
+  input.value = localStorage.getItem("");
+  output.textContent = localStorage.getItem("");
+  
+  function updateOutput() {
+    localStorage.setItem("", input.value);
+    output.textContent = input.value;
+  }
+};
+eightOclock();
+
+
+
+
+
+
